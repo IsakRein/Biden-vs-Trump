4e4defbbcbdc8966991ca8bc5b9414008ebfd0f1
Isak Reinholdsson <isak.reinholdsson@gmail.com>
2020-03-26 21:37:51 +0100

    14

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    private GameManager gameManager;
+    private Transform player;
+
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        player = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        if (player.position.y > 30) {
+            transform.position = new Vector3(0, player.position.y-30, -10);
+        }
+    }
+
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -17,20 +17,23 @@ public class Player : MonoBehaviour
     public float lowJumpMultiplier = 2f;
 
     public bool jumpAvaliable;
-    private int waitFrames;
+    private int frames_collision;
+    private int frames_jump;
     public bool collisionDetected;
 
     private Vector2 lastKnownVelocity;
 
     public GameObject waterSplash;
-
-
+    
     private void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
         rb2d = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
+
+        frames_collision = 100;
+        frames_jump = 100;
     }
 
 
@@ -71,11 +74,12 @@ public class Player : MonoBehaviour
             {
                 rb2d.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
             }
-            if (waitFrames > 2)
-            {
-                collisionDetected = true;
-            }
-            waitFrames++;
+            
+            if (frames_collision == 1) { collisionDetected = true; } 
+            if (frames_jump == 4) { rb2d.velocity = (Vector3.up * jumpingSpeed); } 
+            
+            frames_collision++;
+            frames_jump++;
         }
     }
 
@@ -84,7 +88,7 @@ public class Player : MonoBehaviour
         gameActive = true;
         jumpAvaliable = false;
         collisionDetected = false;
-        transform.position = new Vector2(-gameManager.horizontalSize/2 + 30f, 10f);
+        transform.position = new Vector2(-gameManager.horizontalSize/2 + 35f, 10f);
         rb2d.simulated = true;
         rb2d.velocity = new Vector2(0f,0f);
         animator.enabled = true;
@@ -113,35 +117,16 @@ public class Player : MonoBehaviour
         gameActive = false;
     }
 
-    private void OnCollisionExit2D(Collision2D col)
-    {
-        if (collisionDetected)
-        {
-            if (col.transform.tag == "Level")
-            {
-                jumpAvaliable = false;
-            }
-
-            waitFrames = 0;
-            collisionDetected = false;
-        }
-    }
-
-    void OnCollisionStay2D(Collision2D col)
-    {
-        if (col.transform.tag == "Level")
-        {
-            jumpAvaliable = true;
-        }
-
-        if (col.gameObject.tag == "Death")
-        {
-            animator.SetBool("Death", true);
-        }
-    }
 
     void OnCollisionEnter2D(Collision2D col)
     {
+        Debug.Log("Enter");
+        
+        if (Input.GetKey("space")) 
+        {
+            frames_jump = 0;
+        }
+
         if (collisionDetected)
         {
             if (col.transform.tag == "Level")
@@ -154,7 +139,38 @@ public class Player : MonoBehaviour
                 animator.SetBool("Death", true);
             }
 
-            waitFrames = 0;
+            frames_collision = 0;
+            collisionDetected = false;
+        }
+    }
+
+    
+    void OnCollisionStay2D(Collision2D col)
+    {
+        if (col.transform.tag == "Level")
+        {
+            jumpAvaliable = true;
+        }
+
+        if (col.gameObject.tag == "Death")
+        {
+            animator.SetBool("Death", true);
+        }
+    }
+    
+    
+    void OnCollisionExit2D(Collision2D col)
+    {
+        Debug.Log("Exit");
+
+        if (collisionDetected)
+        {
+            if (col.transform.tag == "Level")
+            {
+                jumpAvaliable = false;
+            }
+
+            frames_collision = 0;
             collisionDetected = false;
         }
     }
