8b66f4122b7121d2bf9b2c7de64ccb7a8915b47a
Isak Reinholdsson <isak.reinholdsson@gmail.com>
2020-06-07 15:13:21 +0200

    68

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -21,7 +21,7 @@ public class CameraScript : MonoBehaviour
     void Update()
     {
         float x = player.position.x + xOffset;
-        float y = 0;
+        float y = 10;
 
         if (player.position.y > yOffset && !playerScript.jetpack_active)
         {
