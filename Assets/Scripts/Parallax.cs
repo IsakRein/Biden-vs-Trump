fe6241cf0a43380d6f33bd6c6d91c4fb0ea4ace9
Isak Reinholdsson <isak.reinholdsson@gmail.com>
2020-06-14 23:46:03 +0200

    76

---
+++ Assets/Scripts/Parallax.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+	public float speed;
+
+	void OnTriggerStay2D() 
+	{
+		transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+	}
+
+	void OnTriggerExit2D() 
+	{
+		Destroy(gameObject);
+	}
+}
