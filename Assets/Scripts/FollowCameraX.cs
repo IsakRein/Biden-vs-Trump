929c54098af2918273a6861090f9d427b59f4e14
Isak Reinholdsson <isak.reinholdsson@gmail.com>
2020-07-02 14:41:21 +0200

    83

---
+++ Assets/Scripts/FollowCameraX.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCameraX : MonoBehaviour
+{
+	public Transform camera_transform; 
+
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, 0,0);
+    }
+}
